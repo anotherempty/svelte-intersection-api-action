d11b65b9445481120a1200683668593baf8b8dbf
anotherempty <anotherempty@outlook.com>
2022-04-20 06:09:15 +0300

    created observer function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+const handle = (entries, node) => {
+  entries.forEach((entry) => {
+    node.dispatchEvent(new CustomEvent("crossed", { detail: entry }));
+  });
+};
+
 export function observer(node, options = {}) {
+  const observer = new IntersectionObserver(
+    (entries) => handle(entries, node),
+    options
+  );
+  observer.observe(node);
+
   return {
-    destroy() {},
+    destroy() {
+      observer.unobserve(node);
+    },
   };
 }
